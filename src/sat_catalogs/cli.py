42d0291029f6eace560252d163530822a52b2483
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-14 13:24:58 -0500

    Add DATABASE argument

--- src/sat_catalogs/cli.py
+++ src/sat_catalogs/cli.py
@@ -4,9 +4,18 @@ import click
 
 
 @click.group()
+@click.argument("database", type=click.File("r"))
 @click.pass_context
-def cli(context: click.Context):
-    """Simple CLI tool to manage SAT's catalog SQL scripts"""
+def cli(context: click.Context, database: click.File):
+    """Simple CLI tool to manage SAT's catalog SQL scripts
+
+    Args:
+        DATABASE: SQLite database file
+    """
     templates_path = os.path.dirname(__file__)
-    context.obj = {"templates_path": templates_path + "/templates"}
+    context.obj = {
+        "templates_path": templates_path + "/templates",
+        "database": database,
+    }
+
 
