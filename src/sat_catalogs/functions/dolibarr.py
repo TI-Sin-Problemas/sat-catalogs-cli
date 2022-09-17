cf070f483b836f77be70123173c5075efa728a9d
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-17 16:30:17 -0500

    Replace semi colon with comma for units of measure descriptions

--- src/sat_catalogs/functions/dolibarr.py
+++ src/sat_catalogs/functions/dolibarr.py
@@ -17,7 +17,7 @@ def get_units_of_measure_sql(db_path: str, templates_path: str) -> str:
     values = []
     for rowid, record in enumerate(records, 1):
         name = record.texto.replace("'", '"')
-        description = record.descripcion.replace("'", '"')
+        description = record.descripcion.replace("'", '"').replace(";", ",")
         values.append(f"    ({rowid}, '{record.id}', '{name}', '{description}', 0)")
 
     with open(f"{templates_path}/units_of_measure.sql", "r", encoding="utf-8") as file:
