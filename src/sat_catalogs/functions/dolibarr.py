976232cc03cf22f67214d9928bababc05007a7f8
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-18 01:18:08 -0500

    Add tax system sql function

--- src/sat_catalogs/functions/dolibarr.py
+++ src/sat_catalogs/functions/dolibarr.py
@@ -4,6 +4,13 @@ from typing import Callable
 from sat_catalogs.orm import SatModel, get_record_scalars
 
 
+def __get_sql(template_path: str, values: str) -> str:
+
+    with open(f"{template_path}", "r", encoding="utf-8") as file:
+        template = file.read()
+    return template.replace("__values__", ",\n".join(values) + ";")
+
+
 def get_dolibarr_function(model: SatModel) -> Callable:
     """Returns the function to call to get the SQL script for a model
 
@@ -22,6 +29,9 @@ def get_dolibarr_function(model: SatModel) -> Callable:
 
         case SatModel.UNIT_OF_MEASURE.name:
             return get_units_of_measure_sql
+
+        case SatModel.TAX_SYSTEM.name:
+            return get_tax_systems_sql
 
         case _:
             raise AttributeError("Invalid model")
@@ -45,10 +55,7 @@ def get_units_of_measure_sql(db_path: str, templates_path: str) -> str:
         description = record.descripcion.replace("'", '"').replace(";", ",")
         values.append(f"    ({rowid}, '{record.id}', '{name}', '{description}', 0)")
 
-    with open(f"{templates_path}/units_of_measure.sql", "r", encoding="utf-8") as file:
-        template = file.read()
-
-    return template.replace("__values__", ",\n".join(values) + ";")
+    return __get_sql(f"{templates_path}/units_of_measure.sql", values)
 
 
 def get_payment_forms_sql(db_path: str, templates_path: str) -> str:
@@ -68,7 +75,24 @@ def get_payment_forms_sql(db_path: str, templates_path: str) -> str:
         name = record.texto.replace("'", '"')
         values.append(f"    ({rowid}, '{record.id}', '{name}', 0)")
 
-    with open(f"{templates_path}/payment_forms.sql", "r", encoding="utf-8") as file:
-        template = file.read()
+    return __get_sql(f"{templates_path}/payment_forms.sql", values)
 
-    return template.replace("__values__", ",\n".join(values) + ";")
+
+def get_tax_systems_sql(db_path: str, templates_path: str) -> str:
+    """Returns the tax system SQL script as string
+
+    Args:
+        db_path (str): Path to the SQLite database file
+        templates_path (str): Path to the scripts template directory
+
+    Returns:
+        str: SQL script
+    """
+    records = get_record_scalars(SatModel.TAX_SYSTEM, db_path)
+
+    values = []
+    for rowid, record in enumerate(records, 1):
+        name = record.texto.replace("'", '"')
+        values.append(f"    ({rowid}, '{record.id}', '{name}', 1)")
+
+    return __get_sql(f"{templates_path}/tax_systems.sql", values)
