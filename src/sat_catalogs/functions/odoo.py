d06219acbd31c23cc60c8420391abe86cafd6b5c
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-11-19 23:43:50 -0600

    Add units of measurement for Odoo

--- src/sat_catalogs/functions/odoo.py
+++ src/sat_catalogs/functions/odoo.py
@@ -22,11 +22,31 @@ def get_odoo_function(model: SatModel) -> Callable:
         function_map = {
             SatModel.TAX_SYSTEM.name: get_tax_systems_csv,
             SatModel.PROD_SERV_KEY.name: get_product_service_keys_csv,
+            SatModel.UNIT_OF_MEASURE.name: get_units_of_measure_csv,
         }
 
         return function_map[model.name]
     except KeyError as err:
         raise AttributeError("Invalid model") from err
+
+
+def get_units_of_measure_csv(db_path: str, templates_path: str) -> str:
+    """Returns the units of measurement CSV as string
+
+    Args:
+        db_path (str): Path to the SQLite database file
+        templates_path (str): Path to the template directory
+
+    Returns:
+        str: CSV string
+    """
+    records = get_record_scalars(SatModel.UNIT_OF_MEASURE, db_path)
+
+    values = []
+    for rowid, record in enumerate(records, 1):
+        values.append(f'unit_of_measure_{rowid:04d},{record.id},"{record.texto}"')
+
+    return get_csv(f"{templates_path}/odoo/units_of_measure.csv", values)
 
 
 def get_tax_systems_csv(db_path: str, templates_path: str) -> str:
